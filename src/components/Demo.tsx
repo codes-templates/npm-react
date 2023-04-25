d9a168e6034dbb7788f32073fc7e60e03f9dc04b
jerrywu <jerrywu001@gmail.com>
2023-04-25 11:55:09 +0800

    feat: use babelrc & broswerslistrc

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
 
+const test = null;
+
 export function Demo() {
-  const [a] = useState(0);
+  const [count] = useState(0);
+  const b = '5'.padStart(2, '1');
+  const c = test ?? 'hello';
+  const d = {} as Record<string, any>;
 
   return (
     <div>
-      count:
-      {' '}
-      {a}
+      <p>
+        count in jsx component:
+        {' '}
+        {count}
+      </p>
+      <p>
+        {b}
+      </p>
+      <p>
+        {c}
+      </p>
+      <p>
+        {d}
+      </p>
     </div>
   );
 }
